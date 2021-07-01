9a4013c00da437bd5930ab5c33dd540511a5d79a
chogoal <goal961@naver.com>
2021-07-01 15:09:13 +0900

    210701 commit 2

---
+++ accountapp/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def hello_world(requests):
+    return HttpResponse('Hello World!') # alt + enter 로 간단히 import
