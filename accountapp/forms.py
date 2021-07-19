024c8f1f2ec6c61d830d27c595ded85cb99caef6
chogoal <goal961@naver.com>
2021-07-19 15:36:46 +0900

    210719 commit 2

---
+++ accountapp/forms.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.forms import UserCreationForm
+
+
+class AccountCreationForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['username'].disabled = True # update info 에서 아이디 변경 불가능
