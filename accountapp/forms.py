e8fc0a150ea7eee348bc0b14f5aa896cbe094c24
chogoal <goal961@naver.com>
2021-07-19 16:01:43 +0900

    210719 commit 3

--- accountapp/forms.py
+++ accountapp/forms.py
@@ -5,4 +5,4 @@ class AccountCreationForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.fields['username'].disabled = True # update info 에서 아이디 변경 불가능
+        self.fields['username'].disabled = True  # update info 에서 아이디 변경 불가능
