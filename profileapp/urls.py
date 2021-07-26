9e8b2f790b896e03a2952213d891484f972a4605
chogoal <goal961@naver.com>
2021-07-26 14:52:50 +0900

    210726 commit 1

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,5 +1,10 @@
+from django.urls import path
 
+from profileapp.views import ProfileCreateView
+
+app_name = 'profileappp'
 
 urlpatterns = [
+    path('create/', ProfileCreateView.as_view(), name='create')
 
 ]
